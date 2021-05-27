6457016ea39880ac5bd656110df706835030053d
Jean Dubois <jd-dev@laposte.net>
2021-05-27 15:30:48 +0200

    Quelques bugs corrigés
    prise en charge (buggée :P) des apostrophes

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ def listToStr(list):
 
 
 def MixSentence(sentence):
+    apostrophesPos = []
+    for pos, letter in enumerate(sentence):
+        apostrophe = False
+        if letter == "'":
+            apostrophe = True
+            apostrophesPos.append(pos)
+        print(letter + " " + str(apostrophe))
+    print(apostrophesPos)
+    sentence = sentence.replace("'", " ")
     sentence = sentence.split()
     mxiedSentence = ''
     for word in sentence:
@@ -20,19 +29,30 @@ def MixSentence(sentence):
             firstLetter = word[:1]  # on récupère la 1ère lettre du txt
             lastLetter = word[len(word) - 1:]  # et sa dernière
             ponctuation = ''
-            while lastLetter == "," or lastLetter == "." or lastLetter == ";" or lastLetter == ":":
+            while lastLetter == "," or lastLetter == "." or lastLetter == ";" or lastLetter == ":" or lastLetter == "?" or lastLetter == "!":
                 # cas des ponctuations
                 ponctuation += lastLetter
                 word = word[:len(word)-1]
                 lastLetter = word[len(word) - 1:]
+            if firstLetter == "?" or firstLetter == "!":
+                firstLetter = None
 
             otherLetters = list(word[1:len(word) - 1])  # on récupère les lettres au milieu dans une liste
+
             random.shuffle(otherLetters)  # qu'on mélange
             otherLetters = listToStr(otherLetters)  # qu'on remet en str
-            mxiedWord = firstLetter + otherLetters + lastLetter  # et on assemble le tout
+            # et on assemble le tout
+            mxiedWord = firstLetter + otherLetters + lastLetter if not firstLetter is None else otherLetters + lastLetter
             mxiedSentence += mxiedWord + ponctuation + " "
         else:  # point-virgule et double-point
             mxiedSentence += word + ' '
+
+    mxiedSentence = list(mxiedSentence)
+    if not len(apostrophesPos) == 0:
+        for apostrophePos in apostrophesPos:
+            mxiedSentence.insert(apostrophePos, "'")
+    mxiedSentence = listToStr(mxiedSentence)
+    
     return mxiedSentence
 
-print(MixSentence("Ceci est un test avec, des ponctuations ; des appostr'ophes : des choses en tout genre... !! ???"))
+print(MixSentence("Ceci est un test avec, des ponctuations ; des apo'str'ophes : des choses en tout genre... !! ??? ??!!"))
